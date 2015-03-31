43f3e2ef7c76a08f229782175816fa5e1e4051f2
nerdaryan <nerdaryan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-31 17:50:52 +0000

    Language files
    git-svn-id: https://plugins.svn.wordpress.org/anspress-question-answer/trunk@1124673 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ widgets/question_stats.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * AnsPress question stats widget
+ * Widget for showing question stats
+ * @package AnsPress
+ * @author Rahul Aryan <rah12@live.com>
+ * @license GPL 2+ GNU GPL licence above 2+
+ * @link http://wp3.in
+ * @since 2.0.0-alpha2
+ *  
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+class AnsPress_Stats_Widget extends WP_Widget {
+
+	public function AnsPress_Stats_Widget() {
+		// Instantiate the parent object
+		parent::__construct( false, __('AnsPress Stats', 'ap') );
+	}
+
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		echo $args['before_widget'];
+		if ( ! empty( $title ) ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+
+		$ans_count 		= ap_count_answer_meta(get_question_id());
+		$last_active 	= ap_last_active(get_question_id());
+		$total_subs 	= ap_post_subscribers_count(get_question_id());
+		$view_count 	= ap_get_qa_views(get_question_id());
+
+
+		echo '<ul class="ap-stats-widget">';
+		echo '<li><span class="stat-label">'.__('Active', 'ap'). '</span><span class="stat-value"><time class="published updated" itemprop="dateUpdated" datetime="'.mysql2date('c', $last_active).'">'.ap_human_time( mysql2date('U', $last_active)).'</time> '.__('Ago', 'ap').'</span></li>' ;
+		echo '<li><span class="stat-label">'.__('Views', 'ap'). '</span><span class="stat-value">'.sprintf(_n('One time', '%d times', $view_count, 'ap'), $view_count).'</span></li>' ;		
+		echo '<li><span class="stat-label">'.__('Answers', 'ap'). '</span><span class="stat-value">'.sprintf(_n('%2$s1%3$s answer', '%2$s%1$d%3$s answers', $ans_count, 'ap'), $ans_count, '<span data-view="answer_count">', '</span>').'</span></li>' ;		
+		echo '<li><span class="stat-label">'.__('Subscribers', 'ap'). '</span><span class="stat-value">'.sprintf(_n('One follower', '%d followers', $total_subs, 'ap'), $total_subs).'</span></li>' ;		
+		echo '</ul>';
+		echo $args['after_widget'];
+	}
+
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'Question stats', 'ap' );
+		}
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+}
+
+function ap_stats_register_widgets() {
+	register_widget( 'AnsPress_Stats_Widget' );
+}
+
+add_action( 'widgets_init', 'ap_stats_register_widgets' );

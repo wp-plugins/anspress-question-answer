8f6253c5cbcad2bdc45ba297b93766cc0572197e
nerdaryan <nerdaryan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-01 04:39:15 +0000

    Releasing 2.3.1
    git-svn-id: https://plugins.svn.wordpress.org/anspress-question-answer/trunk@1190583 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/user.php
+++ includes/user.php
@@ -602,7 +602,7 @@ function ap_user_profile_tab(){
 
     $group = isset($_GET['group']) ? $_GET['group'] : 'basic';
 
-    $link = ap_user_link(false, 'profile');
+    $link = ap_user_link(ap_get_displayed_user_id(), 'profile');
     
     $navs = array(
         'basic' => array('link' => add_query_arg(array('group' => 'basic'), $link), 'title' => __('Basic', 'ap')), 
@@ -799,3 +799,4 @@ function ap_user_link_avatar($user_id, $size = 30){
     echo get_avatar($user_id, $size);
     echo '</a>';
 }
+
